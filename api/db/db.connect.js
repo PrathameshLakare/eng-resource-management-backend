2bdbafeadc827a9cde9d758d95d22486fa0c6835
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-06-05 18:46:39 +0530

    modified: db.connect

--- api/db/db.connect.js
+++ api/db/db.connect.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const initializeDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       maxPoolSize: 10,
       serverSelectionTimeoutMS: 10000,
     });
